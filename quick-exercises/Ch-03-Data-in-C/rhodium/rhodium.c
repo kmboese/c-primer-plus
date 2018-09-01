2301ef9f317aae183d85588cb4d72d94c0eb95ea
Kevin Boese <kmboese@ucdavis.edu>
2018-09-01 11:47:17 -0700

    Added program to convert body weight to value of rhodium, in dollars

---
+++ quick-exercises/Ch-03-Data-in-C/rhodium/rhodium.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+const float POUNDS_TO_TROY_OUNCES = 14.5833f;
+
+int main(void) {
+    float bodyWeight = 0.0f;
+    float rhodiumDollarsPerTroyOunce = 163.886088f / POUNDS_TO_TROY_OUNCES;
+
+
+    printf("Please enter your weight in lbs > ");
+    // %f tells scanf() to read the value as a float. Scanf() always takes a
+    // variable reference as an argument to read the input stream into.
+    scanf("%f", &bodyWeight);
+    printf("The value of your body weight in rhodium is $%.2f\n",
+    bodyWeight*POUNDS_TO_TROY_OUNCES*rhodiumDollarsPerTroyOunce);
+    return 0;
+}
