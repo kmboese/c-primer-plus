946e81118775b8ba960ed6db8d1141b8f7dab388
Kevin Boese <kmboese@ucdavis.edu>
2018-09-01 11:13:35 -0700

    Added printf statement to helloWorld

--- quick-exercises/hello-world/helloWorld.c
+++ quick-exercises/hello-world/helloWorld.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
+    int n, n2, n3;
+    n = 5;
+    n2 = 10;
+    n3 = 13;
     printf("Hello, world!\n");
+    printf("n = %d, n2 = %d, n3 = %d\n", n, n2, n3);
     return 0;
 }
