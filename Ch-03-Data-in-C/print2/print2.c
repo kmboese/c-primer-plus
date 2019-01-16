e0bcbe687a520a89ba185a2973ff14e1d87c15b5
Kevin Boese <kmboese@ucdavis.edu>
2019-01-16 00:39:27 -0800

    Initial commit of print2

---
+++ Ch-03-Data-in-C/print2/print2.c
@@ -0,0 +1,16 @@
+/* More formatting tests with different number types. */
+#include <stdio.h>
+
+int main(void) {
+    unsigned int un = 3000000000; /* system with 32-bit int */
+    short end = 200;
+    long big = 655237;
+    long long verybig= 12345678908642;
+
+    printf("un = %u and not %d\n", un, un);
+    printf("end = %hd and %d\n", end, end); // h is the format specifier for short ints
+    printf("big = %ld and not %hd\n", big, big);
+    printf("verybig = %lld and not %ld\n", verybig, verybig);
+
+    return 0;
+}
