36d869b6cf18cd9c18756405ec03ba06a7487d3d
Kevin Boese <kmboese@ucdavis.edu>
2019-01-16 17:13:23 -0800

    Updated to use boolean type example, included variable sizes in print statements

--- Ch-03-Data-in-C/print2/print2.c
+++ Ch-03-Data-in-C/print2/print2.c
@@ -7,10 +7,24 @@ int main(void) {
     long big = 655237;
     long long verybig= 12345678908642;
 
-    printf("un = %u and not %d\n", un, un);
-    printf("end = %hd and %d\n", end, end); // h is the format specifier for short ints
-    printf("big = %ld and not %hd\n", big, big);
-    printf("verybig = %lld and %ld\n", verybig, verybig);
+    _Bool isOn = 0;
+
+    printf("un = %u and not %d\t\t (sizeof un == %lu bytes)\n", \
+        un, un, sizeof(un));
+    printf("end = %hd and %d\t\t\t\t (sizeof end == %lu bytes)\n", \
+        end, end, sizeof(end)); // h is the format specifier for short ints
+    printf("big = %ld and not %hd\t\t\t (sizeof big == %lu bytes)\n", \
+        big, big, sizeof(big));
+    printf("verybig = %lld and %ld\t (sizeof verybig == %lu bytes)\n", \
+    verybig, verybig, sizeof(verybig));
+
+    if (isOn) {
+        printf("The value of isOn is %d\t (sizeof isOn == %lu)\n",\
+        isOn, sizeof(isOn));
+    }
+    else {
+        printf("isOn is false.\n");
+    }
 
     return 0;
 }
