b4b87b53e8ce3e02d85237b20245df9b348ae1c1
Kevin Boese <kmboese@ucdavis.edu>
2019-01-24 20:28:55 -0800

    Added code for exercise

--- Ch-03-Data-in-C/Exercises/main.c
+++ Ch-03-Data-in-C/Exercises/main.c
@@ -6,7 +6,22 @@
 #define BUFF_LEN 64
 
 void ex01(void) {
+    float underflow = 1.0f;
+    float bigBoi = 3.402823e38;
     printf("Exercise 1:\n");
+    printf("Testing float overflow:\n");
+    printf("The number is originally %f\n", underflow);
+    printf("Dividing %f by %f should yield 3.2864e-39\n", underflow, bigBoi);
+    printf("But due to underflow, the result in C is %f\n", \
+        underflow/bigBoi);
+    printf("After dividing %f by %f 1000 times, the last 10 results are ", \
+        underflow, 100.0f);
+    for (int i = 0; i < 1000; i++) {
+        if (i > 990) {
+            printf("%f, ", underflow/(100.0*i) );
+        }
+    }
+    printf("\n");
 }
 
 int main(void) {
