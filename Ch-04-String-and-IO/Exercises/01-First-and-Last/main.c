31bff5fe39ed54b4dd16ea149997cbd31fb914a9
Kevin Boese <kmboese@ucdavis.edu>
2019-01-26 20:37:48 -0800

    completed exercise 01

--- Ch-04-String-and-IO/Exercises/01-First-and-Last/main.c
+++ Ch-04-String-and-IO/Exercises/01-First-and-Last/main.c
@@ -1,8 +1,23 @@
-/* main.c is a C language program! */
+/* Program that reads in first and last name and prints them in 
+last,first format. */
 #include <stdio.h>
+#include <string.h>
+
+#define BUFF_LEN 64     // length for character buffers
 
 int main(void) {
-    printf("Hello, world!\n");
+    char firstName[BUFF_LEN];
+    char lastName[BUFF_LEN];
+    
+    // Get user inut
+    printf("Please enter your first and last name > ");
+    scanf("%s", firstName);
+    scanf("%s", lastName);
+
+    // Print the names in the desired order
+    printf("Your name in \"first, last\" format is %s, %s\n",\
+        lastName, firstName);
+
 
     return 0;
 }
