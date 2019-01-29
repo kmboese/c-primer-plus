e30893be04a874c42f6f993b61d661f878d6ff20
Kevin Boese <kmboese@ucdavis.edu>
2019-01-28 21:32:49 -0800

    Completed exercise 04

--- Ch-05-Operators-Expressions-Statements/Exercises/04-CM-To-Imperial/cm_to_imp.c
+++ Ch-05-Operators-Expressions-Statements/Exercises/04-CM-To-Imperial/cm_to_imp.c
@@ -1,8 +1,39 @@
 /* main.c is a C language program! */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFF_LEN 64
+#define CM_TO_IN 0.393701
+#define IN_PER_FT 12
 
 int main(void) {
-    printf("Hello, world!\n");
+    char input[BUFF_LEN];
+    double cm = 0.0, totalInches = 0.0;
+    int feet = 0;
+    double inches = 0.0;
+
+    printf("\nEnter a height in centimeters > ");
+    fgets(input, BUFF_LEN, stdin);
+    cm = atof(input);
+    totalInches = cm*CM_TO_IN;
+    feet = (int)totalInches/IN_PER_FT;
+    inches = totalInches - (double)feet*IN_PER_FT;
+
+    while (cm > 0.0) {
+        printf("%.2lf cm = %d feet, %.2lf inches\n", \
+            cm, feet, inches);
+        
+        // Get user input
+        printf("Enter a height in centimeters > ");
+        fgets(input, BUFF_LEN, stdin);
+        cm = atof(input);
+        totalInches = cm*CM_TO_IN;
+        feet = (int)totalInches/IN_PER_FT;
+        inches = totalInches - (double)feet*IN_PER_FT;
+    }
+
+    printf("Goodbye!\n");
 
     return 0;
 }
