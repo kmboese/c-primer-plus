8cd7454e28b1c09810ac3023a8b506cea665fa4a
Kevin Boese <kmboese@ucdavis.edu>
2019-01-29 18:47:20 -0800

    Added insert and print functions

--- Data-Structures/linked-list/main.c
+++ Data-Structures/linked-list/main.c
@@ -2,12 +2,90 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "linked_list.h"
+//#include "linked_list.h"
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+struct LinkedList {
+    struct Node *head;
+    int size;
+};
+
+// Functions
+void init(struct LinkedList *ll);
+void insert(struct LinkedList *ll, int _data);
+void printList(struct LinkedList *ll);
+
 
 int main(void) {
-    struct Node *n = (struct Node*)malloc(sizeof (struct Node));
-    n->data = 5;
+    struct LinkedList *list = \
+        (struct LinkedList *)malloc(sizeof(struct LinkedList));
+    init(list);
+    
+    // Insert some data into the list
+    for (int i = 1; i <= 3; i++) {
+        insert(list, i);
+    }
 
-    printf("Data stored in node n is %d\n", n->data);
+    // Print the list
+    printList(list);
+
     return 0;
+}
+
+void init(struct LinkedList *ll) {
+    // Allocate memoryy for the head
+    ll->head = (struct Node*)malloc(sizeof(struct Node));
+
+    // Fill in data values
+    ll->head->data = -1;
+    ll->head->next = NULL;
+    ll->size = 0;
+}
+
+// LL insert function
+void insert(struct LinkedList *ll, int _data) {
+    // Create a new node
+    struct Node *tmp = (struct Node*)malloc(sizeof(struct Node));
+    tmp->data = _data;
+
+    // Insert node in the list
+    if (ll->size == 0) {
+        tmp->next = NULL;
+        ll->head->next = tmp;
+    }
+    else {
+        // Get previous first node
+        struct Node *first = ll->head->next;
+        ll->head->next = tmp;
+        // Make the new node point to the previous first node
+        tmp->next = first;
+    }
+
+    // Increment the size
+    ll->size++;
+}
+
+// LL print function
+void printList(struct LinkedList *ll) {
+    struct Node *tmp = ll->head;
+    int count = 0;
+
+    // Print contents
+    printf("Linked list contents:\n");
+    while (tmp->next != NULL) {
+        tmp = tmp->next;
+        // Insert newline every 10 values for easier reading
+        if (count != 0 && count % 10 == 0) {
+            printf("\n");
+        }
+        printf("%d ", tmp->data);
+
+        count++;
+    }
+
+    printf("\n");
 }
