b1e5ec2e791c9336ab4341357f66aca3759bd365
Kevin Boese <kmboese@ucdavis.edu>
2019-01-29 19:42:50 -0800

    Removed old code from main

--- Data-Structures/linked-list/main.c
+++ Data-Structures/linked-list/main.c
@@ -4,23 +4,6 @@
 #include <stdlib.h>
 #include "linked_list.h"
 
-// struct Node {
-//     int data;
-//     struct Node *next;
-// };
-
-// struct LinkedList {
-//     struct Node *head;
-//     int size;
-// };
-
-// Functions
-// void init(struct LinkedList *ll);
-// void insert(struct LinkedList *ll, int _data);
-// void printList(struct LinkedList *ll);
-// void destroy(struct LinkedList *ll);
-
-
 int main(void) {
     struct LinkedList *list = malloc(sizeof(struct LinkedList));
     init(list);
