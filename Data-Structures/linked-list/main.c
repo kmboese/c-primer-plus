a423c398f22ea881e3eefcf1be80f5e7f44e8f46
Kevin Boese <kmboese@ucdavis.edu>
2019-01-29 19:21:41 -0800

    Added destroy function

--- Data-Structures/linked-list/main.c
+++ Data-Structures/linked-list/main.c
@@ -18,11 +18,11 @@ struct LinkedList {
 void init(struct LinkedList *ll);
 void insert(struct LinkedList *ll, int _data);
 void printList(struct LinkedList *ll);
+void destroy(struct LinkedList *ll);
 
 
 int main(void) {
-    struct LinkedList *list = \
-        (struct LinkedList *)malloc(sizeof(struct LinkedList));
+    struct LinkedList *list = malloc(sizeof(struct LinkedList));
     init(list);
     
     // Insert some data into the list
@@ -33,11 +33,15 @@ int main(void) {
     // Print the list
     printList(list);
 
+    // Destroy the list
+    destroy(list);
+
     return 0;
 }
 
+// Allocates memory and initializes a Linked List
 void init(struct LinkedList *ll) {
-    // Allocate memoryy for the head
+    // Allocate memory for the head
     ll->head = (struct Node*)malloc(sizeof(struct Node));
 
     // Fill in data values
@@ -46,10 +50,25 @@ void init(struct LinkedList *ll) {
     ll->size = 0;
 }
 
+// Deallocates memory for a linked list
+void destroy(struct LinkedList *ll) {
+    // Deallocate memory for nodes
+    struct Node *curr = ll->head;
+    //struct Node *next = curr->next;
+    while (curr != NULL) {
+        struct Node *victim = curr;
+        curr = curr->next;
+        free(victim);
+    }
+
+    // free the linked list struct
+    free(ll);
+}
+
 // LL insert function
 void insert(struct LinkedList *ll, int _data) {
     // Create a new node
-    struct Node *tmp = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *tmp = malloc(sizeof(struct Node));
     tmp->data = _data;
 
     // Insert node in the list
@@ -71,6 +90,10 @@ void insert(struct LinkedList *ll, int _data) {
 
 // LL print function
 void printList(struct LinkedList *ll) {
+    if (!ll) {
+        fprintf(stderr, "Error: linked list is not initialized!\n");
+        return;
+    }
     struct Node *tmp = ll->head;
     int count = 0;
 
@@ -83,7 +106,6 @@ void printList(struct LinkedList *ll) {
             printf("\n");
         }
         printf("%d ", tmp->data);
-
         count++;
     }
 
