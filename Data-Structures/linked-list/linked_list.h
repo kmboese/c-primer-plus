9d5491d9a639ab930b2a64392b95cbe6c8e7af2d
Kevin Boese <kmboese@ucdavis.edu>
2019-01-29 19:39:54 -0800

    Added comments and a function prototype for find()

--- Data-Structures/linked-list/linked_list.h
+++ Data-Structures/linked-list/linked_list.h
@@ -12,9 +12,17 @@ struct LinkedList {
 };
 
 // Linked list functions
+/* Initializes the memory for the contents of a linked list and sets default 
+values */
 void init(struct LinkedList *ll);
+/* Inserts data after the head of a linked list */
 void insert(struct LinkedList *ll, int _data);
+/* Searches for a given key value in the linked list. Returns the index of the 
+key if it is found, or -1 otherwise. */
+int find(struct LinkedList *ll, int key);
+/* Prints the contents of a linked list */
 void printList(struct LinkedList *ll);
+/* Deallocates memory for a linked list */
 void destroy(struct LinkedList *ll);
 
 #endif
